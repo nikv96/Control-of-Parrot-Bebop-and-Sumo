b85067d628d871712b864fcd331f6fd0d9b67d03
Nikhil Venkatesh <nikv96@gmail.com>
2016-08-19 02:49:55 +0800

    Added dancing sumo code

--- sumo.js
+++ sumo.js
@@ -12,17 +12,16 @@ drone.connect(function() {
     console.log("Drone is in kicker posture.");
   });
 
-  drone.on("postureJumper", function(){
-    console.log("Drone is in jumper posture posture.");
-    drone.animationsHighJump();
-  });
-
   drone.on("ready", function(){
     console.log("Drone is connected and ready!");
   });
 
   drone.on("batteryLow", function(){
     console.log("Battery is low.")
+  });
+
+  drone.on("battery", function(data){      
+    console.log("Battery = " + data.toString());
   });
   drone.on("jumpMotorErrorBlocked", function(){
     console.log("Motor is blocked!")
@@ -32,17 +31,24 @@ drone.connect(function() {
 
   while(t<80000){
     setTimeout(function(){
-      drone.postureJumper();
+      drone.animationsHighJump();
     }, t);
     
     setTimeout(function() {
       drone.backward(50);
     }, t+3000);
+    setTimeout(function(){
+      drone.stop();
+    }, t+4000);
 
-    t = t+1000;
+    t = t+5000;
   }
 
   setTimeout(function() {
     drone.stop();
   }, 82000);
+
+  setTimeout(function(){
+    console.log("Fin!");
+  },83000);
 });
