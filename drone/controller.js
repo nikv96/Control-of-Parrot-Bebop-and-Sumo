fef07b5e10d0ca2e84fe0b936ac2865dc72661d8
Venkatesh <nivenkatesh@paypalcorp.com>
2016-10-11 18:21:43 +0800

    Added Drone Controller

---
+++ drone/controller.js
@@ -0,0 +1,149 @@
+var app = require('express').createServer();
+var io = require('socket.io')(app);
+
+app.listen(3002);
+
+io.on('connection', function (socket) {
+    var bebop = require('node-bebop');
+    var client = bebop.createClient();
+
+    client.PilotingSettings.maxAltitude(10);
+    client.SpeedSettings.maxVerticalSpeed(2);
+
+    setInterval(function(){
+        var batteryLevel = client.battery();
+        socket.emit('event', {name: 'battery', value: batteryLevel});
+    },1000);
+
+    setInterval(function(){
+        var currentAltitude = client.lastAltitude;
+        socket.emit('event', { name: 'altitude',value: currentAltitude});
+    },1000);
+
+    var currentState="";
+
+    socket.on('event', function (data) {
+        if(data.name == 'takeoff'){
+            currentState='takeoff';
+            console.log('Take off');
+            client.takeoff();
+        }
+        if(data.name=='hover'){
+            if(currentState!='hover'){
+                console.log(Hover);
+                client.stop();
+                currentState='hover';
+            }
+        }
+        if(data.name=='land'){
+            currentState='land';
+            console.log('Browser asked Ar Drone to Land');
+            client.land();
+        }
+        if(data.name=='up'){
+            if(currentState!='up'){
+                console.log('Browser asked Ar Drone to gain Altitude');
+                client.up(0.5);
+                currentState='up';
+            }
+        }        
+        if(data.name=='down'){
+            currentState='down';
+            console.log('Browser asked Ar Drone to lose Altitude');
+            client.down(0.5);
+        }
+        if(data.name=='front'){
+            currentState='front';
+            var speed=Math.abs(data.value*1.42);            
+            console.log("Browser asked Ar Drone to go ahead @"+speed*100+"% speed");
+            client.front(speed);
+            client.after(1000,function(){                
+                if(currentState!="hover"){
+                console.log("Browser asked Ar Drone to Stay and Hover");
+                client.stop();
+                currentState="hover";
+                }
+            })
+        }
+        if(data.name=="back"){
+            currentState="back";
+            var speed=Math.abs(data.value*1.42);            
+            console.log("Browser asked Ar Drone to go back @"+speed*100+"% speed");
+            client.back(speed);
+            client.after(1000,function(){                
+                if(currentState!="hover"){
+                console.log("Browser asked Ar Drone to Stay and Hover");
+                client.stop();
+                currentState="hover";
+                }
+            })
+        }
+        if(data.name=="left"){
+            currentState="left";
+            var speed=Math.abs(data.value*1.66);            
+            console.log("Browser asked Ar Drone to go left @"+speed*100+"% speed");
+            client.left(speed);
+            client.after(1000,function(){                
+                if(currentState!="hover"){
+                console.log("Browser asked Ar Drone to Stay and Hover");
+                client.stop();
+                currentState="hover";
+                }
+            })
+        }
+        if(data.name=="right"){
+            currentState="right";
+            var speed=Math.abs(data.value*1.66);            
+            console.log("Browser asked Ar Drone to go right @"+speed*100+"% speed");
+            client.right(speed);
+            client.after(1000,function(){                
+                if(currentState!="hover"){
+                console.log("Browser asked Ar Drone to Stay and Hover");
+                client.stop();
+                currentState="hover";
+                }
+            })
+        }
+        if(data.name=="emergency"){
+            currentState="emergency";
+            console.log("Browser asked Ar Drone to disable emergency");
+            client.disableEmergency();
+        }
+        if(data.name=="flipLeft"){
+            console.log("Browser asked Ar Drone to flip left");
+            client.animate('flipLeft', 1000);
+        }
+        if(data.name=="flipAhead"){
+            console.log("Browser asked Ar Drone to flip ahead");
+            client.animate('flipAhead', 1000);
+        }
+        if(data.name=="flipBehind"){
+            console.log("Browser asked Ar Drone to flip behind");
+            client.animate('flipBehind', 1000);
+        }
+        if(data.name=="flipRight"){
+            console.log("Browser asked Ar Drone to flip right");
+            client.animate('flipRight', 1000);
+        }
+        if(data.name=="clockwise"){
+            console.log("Browser asked Ar Drone to rotate clockwise");
+            client.clockwise(0.5);
+            client.after(1000,function(){                
+                console.log("Browser asked Ar Drone to Stay and Hover");
+                client.stop();
+            })
+        }
+        if(data.name=="counterClockwise"){
+            console.log("Browser asked Ar Drone to rotate counter clockwise");
+            client.counterClockwise(0.5);
+            client.after(1000,function(){                
+                console.log("Browser asked Ar Drone to Stay and Hover");
+                client.stop();
+            })
+        }        
+        if(data.name=="calibrate"){
+            console.log("Browser asked Ar Drone to calibrate");
+            client.calibrate(0);
+        }
+    });
+});
