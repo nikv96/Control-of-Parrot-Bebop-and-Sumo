9dda163d9f359fb792f0bc5fb6f8d4a946456216
Nikhil Venkatesh <nivenkatesh@paypal.com>
2016-10-12 15:16:30 +0800

    Added Server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+    , app = express()
+    , server = require("http").createServer(app)
+
+app.use(express.static(__dirname + '/public'));
+
+
+require("./drone/camera-feed");
+require("./drone/controller");
+
+app.listen(3000);
